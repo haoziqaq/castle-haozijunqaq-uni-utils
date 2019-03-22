9adc40088706ff2e05aa0b305511dd981bb5ddd4
齐晧 <qihao@raybattle.com>
2019-03-22 16:38:16 +0800

    update

---
+++ dist/axios.js
@@ -0,0 +1,181 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _axios = require('axios');
+
+var _axios2 = _interopRequireDefault(_axios);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var getObjectFirstProperty = function getObjectFirstProperty() {
+    var object = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+    for (var key in object) {
+        return [key, object[key]];
+    }
+};
+
+var deleteObjectFirstProperty = function deleteObjectFirstProperty() {
+    var object = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+    for (var key in object) {
+        delete object[key];
+        return;
+    }
+};
+
+var headerExceptRequestURLs = [];
+var headerOptions = [];
+
+var service = _axios2.default.create();
+service.withCredentials = true;
+
+service.defaults.adapter = function (config) {
+    return new Promise(function (resolve, reject) {
+        var url = config.url,
+            data = config.params,
+            header = config.headers,
+            method = config.method;
+
+        uni.request({
+            url: url,
+            header: header,
+            method: method,
+            data: data,
+            success: function success(res) {
+                resolve(res);
+            },
+            fail: function fail(res) {
+                reject(res);
+            }
+        });
+    });
+};
+
+service.interceptors.request.use(function (config) {
+    var hasUrl = headerExceptRequestURLs.some(function (url) {
+        return url === config.url;
+    });
+    if (!hasUrl) {
+        headerOptions.forEach(function (header) {
+            config.headers[header[0]] = header[1];
+        });
+    }
+    return config;
+}, function (error) {
+    Promise.reject(error);
+});
+
+service.getData = function (url, par) {
+    var options = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
+
+    var queryParams = {
+        params: {
+            _t: new Date().getTime()
+        }
+    };
+    if (par) {
+        var params = Object.assign(queryParams.params, par);
+        queryParams = {
+            params: params
+        };
+    }
+    queryParams = Object.assign(queryParams, options);
+    return new Promise(function (resolve, reject) {
+        service.get(url, queryParams).then(function (res) {
+            resolve(res);
+        }).catch(function (error) {
+            reject(error);
+        });
+    });
+};
+
+service.postData = function (url, par) {
+    var options = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
+
+    var queryParams = {
+        params: {
+            _t: new Date().getTime()
+        }
+    };
+    if (par) {
+        var params = Object.assign(queryParams.params, par);
+        queryParams = {
+            params: params
+        };
+    }
+    queryParams = Object.assign(queryParams, options);
+    return new Promise(function (resolve, reject) {
+        service.post(url, null, queryParams).then(function (res) {
+            resolve(res);
+        }).catch(function (error) {
+            reject(error);
+        });
+    });
+};
+
+service.postJSON = function (url, par) {
+    var options = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : { headers: { 'Content-Type': 'application/json' } };
+
+    var queryParams = {
+        params: {
+            _t: new Date().getTime()
+        }
+    };
+    if (par) {
+        var params = Object.assign(queryParams.params, par);
+        queryParams = {
+            params: params
+        };
+    }
+    queryParams = Object.assign(queryParams, options);
+    return new Promise(function (resolve, reject) {
+        service.post(url, null, queryParams).then(function (res) {
+            resolve(res);
+        }).catch(function (error) {
+            reject(error);
+        });
+    });
+};
+
+service.setBaseUrl = function (baseURL) {
+    service.defaults.baseURL = baseURL;
+};
+
+service.getBaseUrl = function (baseURL) {
+    return service.defaults.baseURL;
+};
+
+service.setTimeout = function (time) {
+    service.defaults.timeout = time;
+};
+
+service.getTimeout = function (time) {
+    return service.defaults.timeout;
+};
+
+service.addHeader = function () {
+    var key = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
+    var value = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '';
+
+    headerOptions.push([key, value]);
+};
+
+service.resetHeaders = function () {
+    headerOptions = [];
+};
+
+service.setHeadersExcept = function () {
+    var URLs = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
+
+    headerExceptRequestURLs = URLs;
+};
+
+service.changeIsWithCredentials = function (isWithCredentials) {
+    service.withCredentials = isWithCredentials;
+};
+
+exports.default = service;
---
+++ src/axios.js
@@ -0,0 +1,156 @@
+import axios from 'axios'
+
+const getObjectFirstProperty = (object = {}) => {
+    for (let key in object) {
+        return [key, object[key]];
+    }
+};
+
+const deleteObjectFirstProperty = (object = {}) => {
+    for (let key in object) {
+        delete object[key];
+        return
+    }
+};
+
+let headerExceptRequestURLs = [];
+let headerOptions = [];
+
+const service = axios.create();
+service.withCredentials = true;
+
+service.defaults.adapter = (config) => {
+    return new Promise((resolve, reject) => {
+        const { url , params: data, headers: header, method } = config;
+        uni.request({
+            url,
+            header,
+            method,
+            data,
+            success(res) {
+                resolve(res)
+            },
+            fail(res) {
+                reject(res);
+            },
+        })
+    })
+};
+
+service.interceptors.request.use(config => {
+    let hasUrl = headerExceptRequestURLs.some(url => url === config.url);
+    if (!hasUrl) {
+        headerOptions.forEach((header) => {
+            config.headers[header[0]] = header[1];
+        });
+    }
+    return config;
+}, error => {
+    Promise.reject(error)
+});
+
+service.getData = (url, par, options = {}) => {
+    let queryParams = {
+        params: {
+            _t: new Date().getTime()
+        }
+    };
+    if (par) {
+        let params = Object.assign(queryParams.params, par);
+        queryParams = {
+            params
+        }
+    }
+    queryParams = Object.assign(queryParams, options);
+    return new Promise((resolve, reject) => {
+        service.get(url, queryParams)
+            .then(res => {
+                resolve(res);
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
+};
+
+service.postData = (url, par, options = {}) => {
+    let queryParams = {
+        params: {
+            _t: new Date().getTime()
+        }
+    };
+    if (par) {
+        let params = Object.assign(queryParams.params, par);
+        queryParams = {
+            params
+        }
+    }
+    queryParams = Object.assign(queryParams, options);
+    return new Promise((resolve, reject) => {
+        service.post(url, null, queryParams)
+            .then(res => {
+                resolve(res);
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
+};
+
+service.postJSON = (url, par, options = {headers: {'Content-Type': 'application/json'}}) => {
+    let queryParams = {
+        params: {
+            _t: new Date().getTime()
+        }
+    };
+    if (par) {
+        let params = Object.assign(queryParams.params, par);
+        queryParams = {
+            params
+        }
+    }
+    queryParams = Object.assign(queryParams, options);
+    return new Promise((resolve, reject) => {
+        service.post(url, null, queryParams)
+            .then(res => {
+                resolve(res);
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
+};
+
+service.setBaseUrl = (baseURL) => {
+    service.defaults.baseURL = baseURL;
+};
+
+service.getBaseUrl = (baseURL) => service.defaults.baseURL;
+
+service.setTimeout = (time) => {
+    service.defaults.timeout = time;
+};
+
+service.getTimeout = (time) => service.defaults.timeout;
+
+service.addHeader = (key = '', value = '') => {
+    headerOptions.push([key, value])
+};
+
+service.resetHeaders = () => {
+    headerOptions = [];
+};
+
+service.setHeadersExcept = (URLs = []) => {
+    headerExceptRequestURLs = URLs;
+};
+
+service.changeIsWithCredentials = (isWithCredentials) => {
+    service.withCredentials = isWithCredentials;
+};
+
+
+
+
+
+export default service;
