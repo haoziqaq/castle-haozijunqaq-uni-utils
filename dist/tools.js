adef863939c6b76994b449b3de464474243d2058
齐晧 <qihao@raybattle.com>
2019-07-31 14:54:47 +0800

    update

--- dist/tools.js
+++ dist/tools.js
@@ -3,7 +3,32 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 exports.default = {
+    /**
+     * 函数柯里化
+     * @param fn {function} 需要函数柯里化的函数
+     * @param args 需要被解耦的参数集
+     */
+    $curring: function $curring(fn) {
+        var _this = this;
+
+        for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+            args[_key - 1] = arguments[_key];
+        }
+
+        return function () {
+            for (var _len2 = arguments.length, _args = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+                _args[_key2] = arguments[_key2];
+            }
+
+            return fn.call.apply(fn, [_this].concat(_toConsumableArray(args), _args));
+        };
+    },
+
+
     /**
      * 防抖函数
      * @param method 事件触发的操作
